5b9483cc0653f00a9be8f9b2927b7a4058f114d1
Paraic Mc Donagh <S00188844@mail.itsligo.ie>
2019-10-01 16:00:22 +0100

    exercise 2

--- exercise2/Program.cs
+++ exercise2/Program.cs
@@ -10,6 +10,27 @@ namespace exercise2
     {
         static void Main(string[] args)
         {
+            Car c1 = new Car();
+            c1.Make = "Ford";
+            c1.Model = "Focus";
+            c1.CurrentSpeed = 0;
+            c1.EngineSize = 1.6;
+
+            Car c2 = new Car();
+            c2.Make = "Opel";
+            c2.Model = "Astra";
+            c2.CurrentSpeed = 0;
+            c2.EngineSize = 1.2;
+
+            c1.DisplayCarInfo();
+            Console.WriteLine(c2);
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                c1.Accelerate();
+            }
+            Console.WriteLine(c1);
         }
     }
 }
