885bf0cae6791f062e315bfc4cdd38a4939c9811
Paraic Mc Donagh <S00188844@mail.itsligo.ie>
2019-10-01 16:46:09 +0100

    exercise 4 complete

---
+++ exercise4/CashRegister.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise4
+{
+    class CashRegister
+    {
+        public int NumberOfItems { get; private set; }
+
+        public double total { get; private set; }
+
+
+        public void AddItem(double itemPrice)
+        {
+            total += itemPrice;
+            NumberOfItems++;
+        }
+    }
+}
